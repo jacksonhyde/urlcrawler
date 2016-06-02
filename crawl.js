9d04bed047d86ce4354f28964b21a6085bef4728
Jackson Hyde <jackson.hyde@alliants.com>
2016-06-02 17:22:22 +0100

    Improvements

--- crawl.js
+++ crawl.js
@@ -1,39 +1,16 @@
 var c = require("simplecrawler");
-var crawler = c.crawl("http://put.your.url.here/");
+var crawler = c.crawl("http://you.url.here");
 var fs = require('fs');
-
 
 crawler.interval = 50;
 
 crawler.addFetchCondition(function(parsedURL, queueItem) {
-    return !parsedURL.path.match(/\.pdf/gi);
+    return !parsedURL.path.match(/\.jpg|.png|.gif|.js|.css|.pdf/gi);
 });
 
-crawler.addFetchCondition(function(parsedURL, queueItem) {
-    return !parsedURL.path.match(/\.jpg/gi);
-});
-
-crawler.addFetchCondition(function(parsedURL, queueItem) {
-    return !parsedURL.path.match(/\.png/gi);
-});
-
-crawler.addFetchCondition(function(parsedURL, queueItem) {
-    return !parsedURL.path.match(/\.gif/gi);
-});
-
-crawler.addFetchCondition(function(parsedURL, queueItem) {
-    return !parsedURL.path.match(/\.css/gi);
-});
-
-crawler.addFetchCondition(function(parsedURL, queueItem) {
-    return !parsedURL.path.match(/\.js/gi);
-});
-
-var urls = [];
 var file = fs.createWriteStream('urls.txt');
 
 crawler.on("fetchcomplete", function(queueItem) {
-    // urls.push(queueItem.url);
     file.write(queueItem.url + '\n');
     console.log("Completed fetching resource:", queueItem.url);
 });
